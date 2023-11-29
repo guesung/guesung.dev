dabf4099182f13fe74aa78190d5557ff0c84469b
Kuesung Park <gueit214@naver.com>
2023-11-30 00:50:04 +0900

    docs : site.config 수정

--- site.config.ts
+++ site.config.ts
@@ -1,75 +1,37 @@
 import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
-  // the site's root Notion page (required)
   rootNotionPageId: '53137c48fa9742ef8057c9171da952f5',
-
-
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
-
-  // basic site info (required)
   name: 'guesung blog',
   domain: 'blog.guesung.dev',
   author: 'guesung',
-
-  // open graph metadata (optional)
   description: 'guesung blog - developer blog',
-
-  // social usernames (optional)
-  // twitter: 'transitive_bs',
+  twitter: 'gueit214',
   github: 'guesung',
-  // linkedin: 'fisch2',
+  linkedin: 'guesung',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
   defaultPageIcon:
     null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
-
-  // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: false,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
   isRedisEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
   pageUrlOverrides: null,
-
   includeNotionIdInUrls: true,
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-  navigationStyle: 'default',
+      title: 'Personal Workspace',
+      url: 'https://guesung.oopy.io'
+    },
+  ],
 
-
-  // -------- custom configs (guesung) -------------
-
-  // date-fns format string
   dateformat: 'yyyy년 MM월 dd일',
-
-  // post page - hidden properties
   hiddenPostProperties: ['설명', '상태', '최하위 정렬'],
-
-  // contentPosition (table of contents) text align
   contentPositionTextAlign: 'left',
-
-  // default theme color
   defaultTheme: 'system',
-
-  // enable comment
   enableComment: true,
 });
